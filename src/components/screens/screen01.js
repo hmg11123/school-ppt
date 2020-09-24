0168c5126cb5713603b97fa44ad0438e7f21c1e9
hyj11123 <43002611+hyj11123@users.noreply.github.com>
2020-09-24 11:04:53 +0900

    slider

--- src/components/screens/screen01.js
+++ src/components/screens/screen01.js
@@ -1,32 +1,61 @@
 import React from "react";
-import { Link } from "react-router-dom"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import image01 from "../image/image.png";
 
 class screen01 extends React.Component {
-
-    render() {
-        return (
-            <div>
-                <div className="subHeader">
-                    <ul>
-                        <li >
-                            <Link to="/screen01/subScreen01">1</Link>
-                        </li>
-                        <li >
-                            <Link to="/screen01/subScreen02">2</Link>
-                        </li>
-                        <li>
-                            <Link to="/screen01/subScreen03">3</Link>
-                        </li>
-                    </ul>
-                </div>
-                <div></div>
+  render() {
+    const settings = {
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplaySpeed: 6000,
+    };
+    return (
+      <div className="screen01">
+        <div className="MainSlider">
+          <Slider {...settings}>
+            <div className="slick__box">
+              <div className="screen01_circle"></div>
+              <ul>
+                <li>빅데이터 이용사례</li>
+                <li></li>
+                <li>여가분야</li>
+              </ul>
             </div>
-        )
-    }
-
-
-
+            <div className="slick__box">
+              <ul>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+            <div className="slick__box">
+              <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+            <div className="slick__box">
+              <ul>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+            <div className="slick__box">
+              <ul>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+          </Slider>
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default screen01;
