0cdf6ffcb1ac12d9ded25fc4953f7b646537a268
hyj11123 <43002611+hyj11123@users.noreply.github.com>
2020-09-25 22:42:30 +0900

    Updates

---
+++ precache-manifest.b72ad651d6082f17dfd8d2a6c98a13d9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc0b7b4147eb10379e4873b081406b0",
+    "url": "/school-ppt/index.html"
+  },
+  {
+    "revision": "62030ccf7283a5c818d8",
+    "url": "/school-ppt/static/css/2.8530d09a.chunk.css"
+  },
+  {
+    "revision": "09c6ede462d99aa5b8ab",
+    "url": "/school-ppt/static/css/main.aa988cac.chunk.css"
+  },
+  {
+    "revision": "62030ccf7283a5c818d8",
+    "url": "/school-ppt/static/js/2.672b93ff.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/school-ppt/static/js/2.672b93ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09c6ede462d99aa5b8ab",
+    "url": "/school-ppt/static/js/main.940d8aba.chunk.js"
+  },
+  {
+    "revision": "22e83225b36fb5f2cda1",
+    "url": "/school-ppt/static/js/runtime-main.c36db4e7.js"
+  },
+  {
+    "revision": "4b78e313148a12241c17f9039c1860c6",
+    "url": "/school-ppt/static/media/image.4b78e313.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/school-ppt/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/school-ppt/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/school-ppt/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/school-ppt/static/media/slick.f97e3bbf.svg"
+  }
+]);
