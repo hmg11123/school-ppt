01ab84299a2691ea1c10f02ff5aa69bf33055121
hyj11123 <43002611+hyj11123@users.noreply.github.com>
2020-09-25 22:40:48 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 import { Content, Header } from "./components/layouts";
-import { screen01, screen02, screen03, subScreen01, subScreen02, subScreen03 } from "./components/screens"
-import { Route } from 'react-router-dom';
+import { screen01, screen02, screen03 } from "./components/screens";
+import { Route } from "react-router-dom";
 
 class App extends React.Component {
   render() {
@@ -15,18 +15,9 @@ class App extends React.Component {
           <Route exact path="/screen01" component={screen01} />
           <Route exact path="/screen02" component={screen02} />
           <Route exact path="/screen03" component={screen03} />
-          <Route exact path="/screen01/subScreen01" component={subScreen01} />
-          <Route exact path="/screen01/subScreen02" component={subScreen02} />
-          <Route exact path="/screen01/subScreen03" component={subScreen03} />
-          <Route exact path="/screen02/subScreen01" component={subScreen01} />
-          <Route exact path="/screen02/subScreen02" component={subScreen02} />
-          <Route exact path="/screen02/subScreen03" component={subScreen03} />
-          <Route exact path="/screen03/subScreen01" component={subScreen01} />
-          <Route exact path="/screen03/subScreen02" component={subScreen02} />
-          <Route exact path="/screen03/subScreen03" component={subScreen03} />
         </main>
       </div>
-    )
+    );
   }
 }
 
--- src/components/screens/index.js
+++ src/components/screens/index.js
@@ -1,6 +1,3 @@
-export { default as screen01 } from './screen01';
-export { default as screen02 } from './screen02';
-export { default as screen03 } from './screen03';
-export { default as subScreen01 } from './subScreen01';
-export { default as subScreen02 } from './subScreen02';
-export { default as subScreen03 } from './subScreen03';
+export { default as screen01 } from "./screen01";
+export { default as screen02 } from "./screen02";
+export { default as screen03 } from "./screen03";
